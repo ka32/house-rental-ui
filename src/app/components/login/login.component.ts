7c15d7d3d42b1ccd58da9c02cb4dbb140194e868
Nagesh Kumar <nagesh.andani@gmail.com>
2019-08-13 19:32:40 +0530

    Fix errors in mobile number verification

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -5,6 +5,7 @@ import { ILoginResponse } from './../../models/login-reponse';
 import { Router } from '@angular/router';
 import { Title } from '@angular/platform-browser';
 import { SnackBarService } from 'src/app/services/snack-bar.service';
+declare const AccountKit: any;
 
 @Component({
   selector: 'app-login',
@@ -12,12 +13,16 @@ import { SnackBarService } from 'src/app/services/snack-bar.service';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
   showLoader = false;
 
-  constructor(private authService: AuthService, private router: Router,
-    private constHelper: ConstHelperService, private zone: NgZone,
-    private titleService: Title, private snackBarService: SnackBarService) { }
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+    private constHelper: ConstHelperService,
+    private zone: NgZone,
+    private titleService: Title,
+    private snackBarService: SnackBarService
+  ) {}
 
   ngOnInit() {
     this.titleService.setTitle('Login | ' + this.constHelper.PageTitle);
@@ -40,4 +45,25 @@ export class LoginComponent implements OnInit {
     this.authService.login();
   }
 
+  loginCallback(response) {
+    if (response.status === 'PARTIALLY_AUTHENTICATED') {
+      let code = response.code;
+      let csrf = response.state;
+      // Send code to server to exchange for access token
+    } else if (response.status === 'NOT_AUTHENTICATED') {
+      // handle authentication failure
+    } else if (response.status === 'BAD_PARAMS') {
+      // handle bad parameters
+    }
+  }
+
+  phoneLogin() {
+    const countryCode = '+91'; // document.getElementById('country_code');
+    const phoneNumber = ''; // document.getElementById('phone_number');
+    AccountKit.login(
+      'PHONE',
+      { countryCode: countryCode, phoneNumber: phoneNumber }, // will use default values if not specified
+      this.loginCallback
+    );
+  }
 }
